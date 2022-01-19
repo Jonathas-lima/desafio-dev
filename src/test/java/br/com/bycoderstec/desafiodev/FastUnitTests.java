2ba3ac77f93a6421ee9c82b041043ecdd9f639be
jonathas lima <jonathas.lima@dbccompany.com.br>
2022-01-18 21:57:25 -0300

    Adiciona suite de testes
    Adiciona dependência e classe para executar todos os testes unitários.

---
+++ src/test/java/br/com/bycoderstec/desafiodev/FastUnitTests.java
@@ -0,0 +1,10 @@
+package br.com.bycoderstec.desafiodev;
+
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages({"br.com.bycoderstec.desafiodev.service"})
+public class FastUnitTests {
+}
