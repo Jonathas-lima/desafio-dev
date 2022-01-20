d0610172073738cce7ed80fbe1faa161a42ea085
jonathas lima <jonathas.lima@dbccompany.com.br>
2022-01-20 10:18:35 -0300

    Adiciona novo metodo em JsonUtilsTest
    Adiciona o metodo para converter uma lista JSON em uma lista de objetos java.

--- src/test/java/br/com/bycoderstec/desafiodev/util/JsonUtilsTest.java
+++ src/test/java/br/com/bycoderstec/desafiodev/util/JsonUtilsTest.java
@@ -1,13 +1,25 @@
 package br.com.bycoderstec.desafiodev.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
 
 public class JsonUtilsTest {
 
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     public static <T> T convertToObject(String json, Class<T> tClass) {
+
+        try {
+            return OBJECT_MAPPER.readValue(json, tClass);
+        } catch (JsonProcessingException e) {
+            return null;
+        }
+    }
+
+    public static <T> List<T> convertToListObject(String json, TypeReference<List<T>> tClass) {
 
         try {
             return OBJECT_MAPPER.readValue(json, tClass);
